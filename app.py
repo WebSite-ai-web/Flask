327ad2d30ffaa093f799d8272cc6c51075f76b55
John <meyrzhan04.04@mail.ru>
2025-05-23 22:43:36 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Привет! Это мой сайт на Flask, размещённый на Render.'
+def hello():
+    return 'Привет! Это локальный сервер на 0.0.0.0'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
